87dbd28fd5d599d3e53214f747354f917c82a39d
smetcalf <smetcalf@smetcalfs-MacBook-Pro.local>
2019-08-22 13:40:10 -0400

    auth is finished

--- store/index.js
+++ store/index.js
@@ -3,6 +3,7 @@ import { getUserFromCookie, getUserFromLocalStorage } from "~/utils/auth";
 export default {
   actions: {
     nuxtServerInit({ commit }, { req }) {
+      if (!req) return;
       const user = !process.browser
         ? getUserFromCookie(req)
         : getUserFromLocalStorage();
