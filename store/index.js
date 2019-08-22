dd3affecc55669248a33f936fde4ee101457db77
smetcalf <smetcalf@smetcalfs-MacBook-Pro.local>
2019-08-22 17:36:06 -0400

    auth is finished

--- store/index.js
+++ store/index.js
@@ -7,7 +7,7 @@ export default {
       if (!req.headers.cookie) return;
       const jwtCookie = req.headers.cookie
         .split(";")
-        .find(c => c.trim().startsWith("jwt="));
+        .find(c => c.trim().startsWith("nf_jwt="));
       if (!jwtCookie) return;
       const jwt = jwtCookie.split("=")[1];
 
