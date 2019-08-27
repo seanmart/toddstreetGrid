212636e82fb936c3273104d45a65032083b8fe47
smetcalf <smetcalf@smetcalfs-MacBook-Pro.local>
2019-08-27 16:28:48 -0400

    first copmmit

--- pages/callback.vue
+++ pages/callback.vue
@@ -3,7 +3,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    if (this.$auth.loggedIn) {
+      this.redirect();
+    }
+  },
+  methods: {
+    redirect() {
+      if (this.$route.query.next && isRelativeURL(this.$route.query.next)) {
+        this.$router.push(this.$route.query.next);
+      } else {
+        this.$router.push("/");
+      }
+    }
+  }
+};
 </script>
 
 <style lang="css" scoped></style>
